7c9b8f3ce39aa3809a7fc067fc0726c6aefdd99a
wuwx <wuweixin@gmail.com>
2026-04-07 09:51:03 +0800

    style: 调整 Term 模型中的 use 语句顺序

--- src/Models/Term.php
+++ src/Models/Term.php
@@ -8,8 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\MorphToMany;
-use Kalnoy\Nestedset\NodeTrait;
 use Illuminate\Support\Str;
+use Kalnoy\Nestedset\NodeTrait;
 
 class Term extends Model
 {
